9ce14b01582b6a4113c2dbe6571df9c6a21a49d6
norlen <joacim.norlen@gmail.com>
2021-05-15 14:00:29 +0200

    Fix obscuring element

--- src/test/java/tests/pagemodels/Activity3PageModel.java
+++ src/test/java/tests/pagemodels/Activity3PageModel.java
@@ -23,13 +23,18 @@ public class Activity3PageModel {
         }
     }
 
-    public static void testAdlibris(WebDriver driver) {
+    public static void testAdlibris(WebDriver driver) throws InterruptedException {
         AdlibrisPage startPage = AdlibrisPage.get(driver);
         startPage.inputSearch(searchQuery);
         AdlibrisPage.SearchResultPage resultsPage = startPage.submitSearch();
 
         AdlibrisPage.ProductPage productPage = resultsPage.clickProduct("Praktisk mjukvarutestning");
         productPage.addToCart();
+
+        // The checkout element is initially hidden, so wait a short period for it to become visible.
+        // Prevents error on opera that another element obscures it.
+        Thread.sleep(1000);
+
         productPage.toCheckout();
         driver.quit();
     }
